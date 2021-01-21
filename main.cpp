7b1eae1368d486549089f436c46dde484661d6cc
Yra <48652707+yra77@users.noreply.github.com>
2021-01-21 10:30:35 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "Version: " << version() << std::endl;
+	std::cout << endl << "Hello Word!" << endl;
+
+    return 0;
+}
